579b68c8441b8560af6e8fd71a2feb85a49d4395
Boran Pekel <43743138+boranpek@users.noreply.github.com>
2022-11-06 15:27:11 +1100

    add texts

---
+++ lib/core/constants/texts/texts.dart
@@ -0,0 +1,24 @@
+class AppTexts {
+  static const registerScreenHeader = "Let's Begin";
+  static const loginScreenHeader = 'Log in';
+  static const forgotPassScreenHeader = 'Forgot Password';
+  static const nameHintText = 'Name*';
+  static const emailHintText = 'Email*';
+  static const passwordHintText = 'Password*';
+  static const confirmPassHintText = 'Confirm password*';
+  static const genderHintText = 'Gender';
+  static const birthHintText = 'Year of birth';
+  static const privacyPolicyText =
+      'I have read the Terms of Use and Privacy Policy and I want to proceed.';
+  static const alreadyHaveProfile = 'Already have a profile?';
+  static const saveButton = 'Save';
+  static const loginButton = 'Login';
+  static const continueButton = 'Continue';
+  static const cancelButton = 'Cancel';
+  static const letsGoButton = "Let's go";
+  static const registerButton = 'Register';
+  static const forgotYourPassword = 'Forgot your password';
+  static const dontHaveAccount = "Don't have an account? Register now!";
+  static const fotgotPassDescription =
+      "Enter the email address associated with your account and we'll email you a link to reset your password.";
+}
