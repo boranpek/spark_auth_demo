5ab33cef844e643e52c411df85b8f7b05a99d840
Boran Pekel <43743138+boranpek@users.noreply.github.com>
2022-11-06 15:08:24 +1100

    customize colors

--- lib/core/init/theme/color/app_colors.dart
+++ lib/core/init/theme/color/app_colors.dart
@@ -1,5 +1,41 @@
-abstract class AppColors {}
+import 'package:flutter/material.dart';
+import 'package:spark_auth_demo/core/extensions/string_extensions.dart';
 
-class LightColors extends AppColors {}
+abstract class AppColors {
+  Color nasuPurple = '6624CB'.color;
+  Color periwinkle = '8C7BFF'.color;
+  Color slaaneshGrey = 'DCD4E7'.color;
+  Color trumpeter = '8D7AAA'.color;
 
-class DarkColors extends AppColors {}
+  late Color primaryColor;
+  late Color primaryButtonColor;
+  late Color secondaryButtonColor;
+  late Color borderColor;
+  late Color textColor;
+}
+
+class LightColors extends AppColors {
+  @override
+  Color get primaryColor => nasuPurple;
+  @override
+  Color get primaryButtonColor => nasuPurple;
+  @override
+  Color get secondaryButtonColor => periwinkle;
+  @override
+  Color get borderColor => slaaneshGrey;
+  @override
+  Color get textColor => trumpeter;
+}
+
+class DarkColors extends AppColors {
+  @override
+  Color get primaryColor => nasuPurple;
+  @override
+  Color get primaryButtonColor => nasuPurple;
+  @override
+  Color get secondaryButtonColor => periwinkle;
+  @override
+  Color get borderColor => slaaneshGrey;
+  @override
+  Color get textColor => trumpeter;
+}
