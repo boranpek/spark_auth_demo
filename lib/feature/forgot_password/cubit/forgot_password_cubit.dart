7ce2eb2c9df081963bea707bc46d15da5b9bfecb
Boran Pekel <43743138+boranpek@users.noreply.github.com>
2022-11-07 01:37:02 +1100

    handle forgat pass errors

--- lib/feature/forgot_password/cubit/forgot_password_cubit.dart
+++ lib/feature/forgot_password/cubit/forgot_password_cubit.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:spark_auth_demo/core/base/cubit/base_cubit.dart';
+import 'package:spark_auth_demo/core/extensions/context_extensions.dart';
 
 import '../../../core/constants/navigation/navigation_constants.dart';
 
@@ -24,11 +25,89 @@ class ForgotPasswordCubit extends Cubit<ForgotPasswordState> with BaseCubit {
   }
 
   Future resetPassword() async {
-    try {
-      await FirebaseAuth.instance
-          .sendPasswordResetEmail(email: emailController.text);
-    } on FirebaseAuthException catch (e) {
-      print(e);
+    if (state.isButtonActive) {
+      showDialog(
+        context: context!,
+        builder: (ctx) => AlertDialog(
+          title: Text(
+            'Please wait...',
+            textAlign: TextAlign.center,
+            style: context!.textTheme.headline2,
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: CircularProgressIndicator(
+                    color: context!.appColors.primaryColor),
+              ),
+            ],
+          ),
+        ),
+      );
+      try {
+        await FirebaseAuth.instance
+            .sendPasswordResetEmail(email: emailController.text);
+        Navigator.pop(context!);
+        context!.showSnackBar('Email is sent');
+        await Future.delayed(const Duration(seconds: 2));
+        navigateToLogin();
+      } on FirebaseAuthException {
+        Navigator.pop(context!);
+        showDialog(
+          context: context!,
+          builder: (ctx) => AlertDialog(
+            title: Text(
+              'Wrong email',
+              textAlign: TextAlign.center,
+              style: context!.textTheme.headline2,
+            ),
+            actions: <Widget>[
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'OK',
+                    textAlign: TextAlign.center,
+                    style: context!.textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    } else {
+      showDialog(
+          context: context!,
+          builder: (ctx) => AlertDialog(
+            title: Text(
+              'Please enter your email',
+              textAlign: TextAlign.center,
+              style: context!.textTheme.headline2,
+            ),
+            actions: <Widget>[
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop();
+                  },
+                  child: Text(
+                    'OK',
+                    textAlign: TextAlign.center,
+                    style: context!.textTheme.bodyText1,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
     }
+  }
+
+  isButtonActive(bool isError) {
+    emit(state.copyWith(isButtonActive: !isError));
   }
 }
